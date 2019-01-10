976f335fbfd37d79cbeec06f9fe6ae536b35dfaf
serj <serj@serj-VirtualBox>
2019-01-10 09:37:10 +0200

    import object

--- backend/modules/admin/controllers/SimController.php
+++ backend/modules/admin/controllers/SimController.php
@@ -98,7 +98,7 @@ class SimController extends Controller
                     }else{
                         $model = new \backend\modules\guard\models\BgModel;
                         $model->name_model = trim($rowData[0][0]);
-                        $model->id_marka = 43;
+                        $model->id_marka = 6;
                         if(!$model->save()) print_r($model->errors);
                     }
                     //if(!$unit->save()) print_r($unit->errors);
